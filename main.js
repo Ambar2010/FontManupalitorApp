57c24b65366db4d6e7953be344a0c77263a2c78d
Ambar2010 <89517719+Ambar2010@users.noreply.github.com>
2022-06-01 08:07:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
     canvas = createCanvas(550,550);
     canvas.position(800,100);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw() {
+    background('#ff3f05');
+}
+function modelLoaded() {
+    console.log("Model Is Initialized");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+   }
 }
